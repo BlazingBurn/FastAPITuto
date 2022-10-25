f9a8688b86ff1d36c269f1dadc1fd0f943f1aa8c
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2022-10-25 14:11:23 +0200

    fin tuto jusqu'a 7

---
+++ bodyMultipleParamTest.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, Path, Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+# First test Path
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     *,
+#     item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+#     q: str | None = None,
+#     item: Item | None = None,
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item:
+#         results.update({"item": item})
+#     return results
+
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
+# test with two model
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item, user: User):
+#     results = {"item_id": item_id, "item": item, "user": user}
+#     return results
+
+
+# use Body
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item, user: User, importance: int = Body(), q: str | None = None):
+#     results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+# use embed
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item = Body(embed=True)):
+    results = {"item_id": item_id, "item": item}
+    return results
